9ec6ee63401de90f34db855b9ef76bfba11348b9
Franck Mukendi <franck.mukendi@gmail.com>
2018-03-17 10:35:33 -0400

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
+
+// Angular System Module 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
+// External Module 
+import { AngularFireModule } from 'angularfire2';
 
+// My Modules 
+
+// My Compononent
 import { AppComponent } from './app.component';
 
+// My Services
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDyIyUcHHKgMN0DAnPnWbrS6hCyGBEyakk",
+  authDomain: "mukehouse-2fdc3.firebaseapp.com",
+  databaseURL: "https://mukehouse-2fdc3.firebaseio.com",
+  projectId: "mukehouse-2fdc3",
+  storageBucket: "mukehouse-2fdc3.appspot.com",
+  messagingSenderId: "131277299799"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    // Angular Modules
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    // External Modules
+    AngularFireModule.initializeApp(firebaseConfig)
+    // My Modules
   ],
   providers: [],
   bootstrap: [AppComponent]
